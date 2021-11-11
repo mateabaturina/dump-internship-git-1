59ff20ff75776cbafe20afa479dadf200c4e4a10
matea baturina <mbaturina@pmfst.hr>
2021-11-11 14:37:28 +0100

    Dodala tri file-a

---
+++ Tic Tac Toe3.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Tic_Tac_Toe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("[ime_igrača] ima x pobjeda i y poraza!");
+            Console.WriteLine("[ime_igrača] ima z pobjeda i n poraza!");
+
+            Console.WriteLine("  |  |  ");
+            Console.WriteLine("  |  |  ");
+            Console.WriteLine("  |  |  ");
+        }
+    }
+}
