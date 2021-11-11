97df988c1c63fbcb9473fcb397fe299562d8eb7c
antissimo <boskovicante1@gmail.com>
2021-11-11 14:07:49 +0100

    Napravija 3 c# filea :wq

---
+++ TrecaIgra.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace KrizicKruzicTrecaIgra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Prva osoba ima 0 pobjeda i 0 poraza");
+            Console.WriteLine("Druga osoba ima 0 pobjeda i 0 poraza");
+            Console.WriteLine(" | | ");
+            Console.WriteLine(" | | ");
+            Console.WriteLine(" | | ");
+        }
+    }
+}
