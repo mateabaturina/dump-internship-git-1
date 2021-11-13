a29b7d7f2fd92b19cffd4edb5e109303db22404e
matea baturina <mbaturina@pmfst.hr>
2021-11-13 18:44:11 +0100

    Odigrani potezi

--- DrugaIgra.cs
+++ DrugaIgra.cs
@@ -8,7 +8,7 @@ namespace KrizicKruzicDrugaIgra
         {
             Console.WriteLine("Prva osoba ima 0 pobjeda i 0 poraza");
             Console.WriteLine("Druga osoba ima 0 pobjeda i 0 poraza");
-            Console.WriteLine("o| |o");
+            Console.WriteLine("o|o|o");
             Console.WriteLine("x|x| ");
             Console.WriteLine("x| | ");
         }
