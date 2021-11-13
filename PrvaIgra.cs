709af38164bf56f7deb1b407676e39949e474343
matea baturina <mbaturina@pmfst.hr>
2021-11-13 18:52:56 +0100

    Kraj igre!

--- PrvaIgra.cs
+++ PrvaIgra.cs
@@ -6,8 +6,8 @@ namespace KrizicKruzicPrvaIgra
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Prva osoba ima 0 pobjeda i 0 poraza");
-            Console.WriteLine("Druga osoba ima 0 pobjeda i 0 poraza");
+            Console.WriteLine("Prva osoba ima 1 pobjeda i 2 poraza");
+            Console.WriteLine("Druga osoba ima 2 pobjeda i 1 poraza");
             Console.WriteLine(" |x| ");
             Console.WriteLine(" |x| ");
             Console.WriteLine("o|x|o");
--- TrecaIgra.cs
+++ TrecaIgra.cs
@@ -6,8 +6,8 @@ namespace KrizicKruzicTrecaIgra
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Prva osoba ima 0 pobjeda i 0 poraza");
-            Console.WriteLine("Druga osoba ima 0 pobjeda i 0 poraza");
+            Console.WriteLine("Prva osoba ima 1 pobjeda i 2 poraza");
+            Console.WriteLine("Druga osoba ima 2 pobjeda i 1 poraza");
             Console.WriteLine("o|x| ");
             Console.WriteLine("o|x| ");
             Console.WriteLine("o| |x");
