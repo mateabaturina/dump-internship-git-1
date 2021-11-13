e386447358bc8e00518ca8667313d7a3ee0e2c63
matea baturina <mbaturina@pmfst.hr>
2021-11-13 17:02:31 +0100

    Odigran novi potez

--- PrvaIgra.cs
+++ PrvaIgra.cs
@@ -10,7 +10,7 @@ namespace KrizicKruzicPrvaIgra
             Console.WriteLine("Druga osoba ima 0 pobjeda i 0 poraza");
             Console.WriteLine(" |x| ");
             Console.WriteLine(" | | ");
-            Console.WriteLine(" | | ");
+            Console.WriteLine("o| | ");
         }
     }
 }
